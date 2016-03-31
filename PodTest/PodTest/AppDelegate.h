81a6cb1f50f4a018952b56ed9bf6fca26c9bf4c0
杜甲 <815319775@qq.com>
2016-03-31 08:47:58 +0800

    pod

---
+++ PodTest/PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by 杜甲 on 16/3/31.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by 杜甲 on 16/3/31.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
